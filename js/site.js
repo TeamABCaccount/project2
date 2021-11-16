33a0ad3e1b20afd831954e78ab9a99fce3da32d1
Angie <77899962+aciturbide@users.noreply.github.com>
2021-11-16 14:59:02 -0600

    broken code delete

--- js/site.js
+++ js/site.js
@@ -41,31 +41,3 @@ document.getElementById("terms").addEventListener("click", function(didread) {
 
     }
   /* slideshow js, advancing to next picuture, non-automatic advancement */
-  var slideIndex = 1;
-  showSlides(slideIndex);
-
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  }
